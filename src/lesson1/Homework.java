e097083b46d1346f8c202754e4555a3638b8f585
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-10-25 18:19:04 +0300

    Add homework

---
+++ src/lesson1/Homework.java
@@ -0,0 +1,12 @@
+package lesson1;
+
+public class Homework {
+    public static void main(String[] args) {
+        System.out.printf("%20s\n", "РУБАИ");
+        System.out.println("Много лет размышлял я над жизнью земной." +
+                "\nНепонятного нет для меня под луной." +
+                "\nМне известно, что мне ничего не известно!" +
+                "\nВот последняя правда, открытая мной.");
+        System.out.printf("%40s", "О.Хайям");
+    }
+}
