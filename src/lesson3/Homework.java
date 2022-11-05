7f647374bcfadc94db68dbcc95aafcde8304538d
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-11-05 11:13:37 +0300

    Lesson 3 - Add homework

---
+++ src/lesson3/Homework.java
@@ -0,0 +1,15 @@
+package lesson3;
+
+@SuppressWarnings("ConstantConditions")
+public class Homework {
+    public static void main(String[] args) {
+        int i1 = 5, i2 = 11;
+        double d1 = 5.5, d2 = 1.3;
+        long l = 20L;
+
+        System.out.println("1.\n" + "i2 / d1 + d2 % i1 - l = " + (i2 / d1 + d2 % i1 - l) + "\n2.");
+        int a = 5, b = 8;
+        System.out.println("a-- - --a + ++a + a++ + a = " + (a-- - --a + ++a + a++ + a));
+        System.out.println("++b - b++ + ++b - --b = " + (++b - b++ + ++b - --b));
+    }
+}
