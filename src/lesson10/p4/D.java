bb903907acbd3250d78c0de9b7ec7aa704948112
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-11-20 22:48:17 +0300

    Lesson 10 - Work on mistakes

--- src/lesson10/p4/D.java
+++ src/lesson10/p4/D.java
@@ -1,9 +1,26 @@
 package lesson10.p4;
 
-public class D {
-    public String d = "non-static variable of D";
+import lesson10.p1.A;
+import lesson10.p1.p2.p3.C;
+import lesson10.p4.p5.E;
 
-    public String getD() {
-        return "non-static method of D";
+import static lesson10.p1.p2.B.*;
+
+public class D {
+    public static void main(String[] args) {
+        A a = new A();
+        System.out.println(a.a);
+        System.out.println(a.getA());
+
+        System.out.println(b);
+        System.out.println(getB());
+
+        C c = new C();
+        System.out.println(c.c);
+        System.out.println(c.getC());
+
+        E e = new E();
+        System.out.println(e.e);
+        System.out.println(e.getE());
     }
 }
--- src/lesson10/p4/p5/E.java
+++ src/lesson10/p4/p5/E.java
@@ -1,26 +1,9 @@
 package lesson10.p4.p5;
 
-import lesson10.p1.*;
-import lesson10.p1.p2.p3.C;
-import lesson10.p4.D;
-
-import static lesson10.p1.p2.B.*;
-
 public class E {
-    public static void main(String[] args) {
-        A a = new A();
-        System.out.println(a.a);
-        System.out.println(a.getA());
+    public String e = "non-static variable of E";
 
-        System.out.println(b);
-        System.out.println(getB());
-
-        C c = new C();
-        System.out.println(c.c);
-        System.out.println(c.getC());
-
-        D d = new D();
-        System.out.println(d.d);
-        System.out.println(d.getD());
+    public String getE() {
+        return "non-static method of E";
     }
 }
