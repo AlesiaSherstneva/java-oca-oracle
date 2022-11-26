24191def6351eb7412763fa10f8dbcddb2ebc08b
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-11-26 17:17:37 +0300

    Lesson 12 - Add the solution

--- src/lesson12/Student.java
+++ src/lesson12/Student.java
@@ -10,10 +10,37 @@ public class Student {
         this.course = course;
         this.grade = grade;
     }
+
+    @Override
+    public String toString() {
+        return name + ", " + course + " course, grade=" + grade;
+    }
 }
 
 class StudentTest {
     public static void main(String[] args) {
+        Student student1 = new Student("Ivan", 4, 6.52);
+        Student student2 = new Student("Ivan", 3, 7.41);
+        Student student3 = new Student("Ivan", 4, 5.94);
+        Student student4 = new Student("Petr", 4, 6.52);
+        Student student5 = new Student("Ivan", 4, 6.52);
+
+        System.out.println(student1 + " and " + student2);
+        equalsWithIf(student1, student2);
+        equalsWithNestedIf(student1, student2);
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println(student1 + " and " + student3);
+        equalsWithIf(student1, student3);
+        equalsWithNestedIf(student1, student3);
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println(student1 + " and " + student4);
+        equalsWithIf(student1, student4);
+        equalsWithNestedIf(student1, student4);
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println(student1 + " and " + student5);
+        equalsWithIf(student1, student5);
+        equalsWithNestedIf(student1, student5);
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
     }
 
     public static void equalsWithIf(Student student1, Student student2) {
