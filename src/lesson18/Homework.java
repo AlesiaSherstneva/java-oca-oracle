6fc33d9253b98c318f283ba1463da53cc3147b19
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-12-15 17:07:34 +0300

    Lesson 18 - Add second method

--- src/lesson18/Homework.java
+++ src/lesson18/Homework.java
@@ -4,8 +4,14 @@ import java.util.Arrays;
 
 public class Homework {
     public static void main(String[] args) {
-        int[] array = {1, 6, -4, 0, 456, -17, 74, 2};
-        System.out.println(Arrays.toString(sorting(array)));
+        System.out.println("1.");
+        int[] intArray = {1, 6, -4, 0, 456, -17, 74, 2};
+        System.out.println("Unsorted array:\n" + Arrays.toString(intArray));
+        System.out.println("Sorted array:\n" + Arrays.toString(sorting(intArray)));
+
+        System.out.println("2.");
+        String[][] stringArray = {{"element00", "element01", "element02"}, {"element10", "element11", "element12"}};
+        showArray(stringArray);
     }
 
     // метод sortirovka (обычный bubble sort)
@@ -23,5 +29,20 @@ public class Homework {
             }
         }
         return arrayToSort;
+    }
+
+    static void showArray(String[][] arrayToShow) {
+        StringBuilder output = new StringBuilder("{ ");
+        for (int i = 0; i < arrayToShow.length; i++) {
+            output.append("{");
+            for (int j = 0; j < arrayToShow[i].length; j++) {
+                output.append(arrayToShow[i][j]);
+                if (j != arrayToShow[i].length - 1) output.append(", ");
+            }
+            output.append("}");
+            if (i != arrayToShow.length - 1) output.append(", ");
+        }
+        output.append(" }");
+        System.out.println(output);
     }
 }
